e96d1f92373fd78fabb9b583b82b2d55fbecb774
Katrina Brinson <kbrinso@gmail.com>
2018-01-25 18:22:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(list)
-  rmls = list.each{|person| "Hello, #{person}! You'll be assigned to room #{person.index + 1}!"}
+  rmls = list.each{|person| "Hello, #{person}! You'll be assigned to room #{list.index(person) + 1}!"}
   return rmls
 end
