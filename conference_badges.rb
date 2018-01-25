4495b4353d551bc9d3ee2f2adacfbb9e5390dd0f
Katrina Brinson <kbrinso@gmail.com>
2018-01-25 18:09:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  array.each{|person| badge_maker(person)}
+end
+
+def assign_rooms(list)
+  rmls = list.each{|person|
+end
